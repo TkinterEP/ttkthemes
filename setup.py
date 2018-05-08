355e4cc1f9e82dcfa3526866e50f168f55920fb6
RedFantom <redfantom@outlook.com>
2018-05-08 09:35:17 +0200

    Bump version number to 2.0.6

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     name='ttkthemes',
     packages=['ttkthemes'],
     package_data={"ttkthemes": ["themes/*", "tkimg/*"]},
-    version='2.0.5',
+    version='2.0.6',
     description='A group of themes for the ttk extensions of Tkinter with a Tkinter.Tk wrapper',
     author='The ttkthemes authors',
     author_email='redfantom@outlook.com',
