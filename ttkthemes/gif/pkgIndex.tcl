7b36f66abbbc549faccef05b3c65a6e9a31d493e
RedFantom <redfantom@outlook.com>
2018-07-09 16:33:51 +0200

    Fix path finding in TCL code files

--- ttkthemes/gif/pkgIndex.tcl
+++ ttkthemes/gif/pkgIndex.tcl
@@ -1,5 +1,6 @@
 # Author: RedFantom
 # License: GNU GPLv3
 # Copyright (c) 2017-2018 RedFantom
+set themesdir [file join [pwd] [file dirname [info script]]]
 source [file join $themesdir arc arc.tcl]
 source [file join $themesdir ubuntu ubuntu.tcl]
