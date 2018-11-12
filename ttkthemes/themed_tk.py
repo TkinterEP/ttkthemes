48f041be95fcbcdc82c1e0c67e0667e00203c095
Maicol <maicolbattistini@live.it>
2018-11-12 20:07:26 +0100

    Fixed background option for window and elements

--- ttkthemes/themed_tk.py
+++ ttkthemes/themed_tk.py
@@ -50,7 +50,10 @@ class ThemedTk(tk.Tk, ThemedWidget):
         """Redirect the set_theme call to also set Tk background color"""
         ThemedWidget.set_theme(self, theme_name)
         color = ttk.Style(self).lookup("TFrame", "background", default="white")
-        if background is True:
+        if bool(background) is True:
+            self.config(background=background)
+            ttk.Style(self).configure(".", background=background)
+        else:
             self.config(background=color)
         if toplevel is True:
             self._setup_toplevel_hook(color)
