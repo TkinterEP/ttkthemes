19f8aea8c7347bdf14d301219269a5814823ba38
RedFantom <redfantom@outlook.com>
2021-02-15 13:43:45 +0100

    Bump version number to v3.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'The ttkthemes authors'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = 'v3.1.1'
+release = 'v3.2.1'
 
 
 # -- General configuration ---------------------------------------------------
